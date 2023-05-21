51bab2daf3b60c4561c27cf5cdec46f54bd4d4a4
mohit <rajpootmohit782@gmail.com>
2023-05-21 05:28:52 +0000

    encryption Update 4 files

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 exports.signup = (req, res) => {
@@ -8,23 +9,31 @@ exports.signup = (req, res) => {
   User.findOne({ where: { email } })
     .then((existingUser) => {
       if (existingUser) {
-        return res
-          .status(409)
-          .json({
-            message: "Email already exists, please choose a different email",
-          });
+        return res.status(409).json({
+          message: "Email already exists, please choose a different email",
+        });
       }
 
-      // Create a new user in the database
-      User.create({ name, email, password })
-        .then((user) => {
-          // Send a response back to the client
-          res.status(201).json({ message: "User created successfully", user });
-        })
-        .catch((error) => {
-          console.log(error);
-          res.status(500).json({ message: "An error occurred" });
-        });
+      // Hash the password
+      bcrypt.hash(password, 10, (err, hashedPassword) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({ message: "An error occurred" });
+        }
+
+        // Create a new user in the database with the hashed password
+        User.create({ name, email, password: hashedPassword })
+          .then((user) => {
+            // Send a response back to the client
+            res
+              .status(201)
+              .json({ message: "User created successfully", user });
+          })
+          .catch((error) => {
+            console.log(error);
+            res.status(500).json({ message: "An error occurred" });
+          });
+      });
     })
     .catch((error) => {
       console.log(error);
@@ -35,20 +44,28 @@ exports.signup = (req, res) => {
 exports.login = (req, res) => {
   // Extract and validate request data
   const { email, password } = req.body;
-  // Perform login logic, e.g., check if user exists and verify password
+
+  // Find the user by email
   User.findOne({ where: { email } })
     .then((user) => {
       if (!user) {
         return res.status(404).json({ message: "User not found" });
       }
 
-      // Check password validity, you can use bcrypt or any other library for password hashing/verification
-      if (user.password !== password) {
-        return res.status(401).json({ message: "Invalid password" });
-      }
+      // Compare the entered password with the hashed password stored in the database
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({ message: "An error occurred" });
+        }
 
-      // User authentication successful
-      res.json({ message: "Login successful", user });
+        if (!result) {
+          return res.status(401).json({ message: "Invalid password" });
+        }
+
+        // User authentication successful
+        res.json({ message: "Login successful", user });
+      });
     })
     .catch((error) => {
       console.log(error);
